5ba7b42006f537fad5ef9fabc1a7b2ec46e14aa6
Gastón Rial Saibene <gars@boozt.com>
2024-01-30 10:06:29 +0100

    Using WithIAMAuthNTokenSources for authentication instead

--- mysql/provider.go
+++ mysql/provider.go
@@ -184,10 +184,10 @@ func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}
 		endpoint = strings.ReplaceAll(endpoint, "cloudsql://", "")
 		var err error
 		if iam_auth { // Access token will be in the password field
-			token_opetion := cloudsqlconn.WithTokenSource(oauth2.StaticTokenSource(&oauth2.Token{
+			token := oauth2.StaticTokenSource(&oauth2.Token{
 				AccessToken: password,
-			}))
-			_, err = cloudsql.RegisterDriver("cloudsql", token_opetion, cloudsqlconn.WithIAMAuthN())
+			})
+			_, err = cloudsql.RegisterDriver("cloudsql", cloudsqlconn.WithIAMAuthNTokenSources(token, token))
 		} else {
 			_, err = cloudsql.RegisterDriver("cloudsql")
 		}
